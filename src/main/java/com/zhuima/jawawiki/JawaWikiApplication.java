09b8807b798dc43c08157914a5629baac248e213
zhuima <zhuima314@gmail.com>
2022-09-07 18:47:27 +0800

    init project

---
+++ src/main/java/com/zhuima/jawawiki/JawaWikiApplication.java
@@ -0,0 +1,23 @@
+package com.zhuima.jawawiki;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class JawaWikiApplication {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(JawaWikiApplication.class);
+    public static void main(String[] args) {
+//        SpringApplication.run(JawawikiApplication.class, args);
+        SpringApplication app = new SpringApplication(JawaWikiApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
+    }
+
+}
---
+++ src/test/java/com/zhuima/jawawiki/JawawikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuima.jawawiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JawawikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
