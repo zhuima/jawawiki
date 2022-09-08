e00f632037c72b0753cbb0bc32a813ed89af92eb
zhuima <zhuima314@gmail.com>
2022-09-08 13:17:04 +0800

    add hello world api

---
+++ src/main/java/com/zhuima/jawawiki/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.zhuima.jawawiki.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
