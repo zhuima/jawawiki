52c0426191f6f303321f940bb50e6a67b190a302
zhuima <zhuima314@gmail.com>
2022-09-09 10:57:25 +0800

    3.8 引入lombok, 简化Req和Resp代码

--- src/main/java/com/zhuima/jawawiki/resp/CommonResp.java
+++ src/main/java/com/zhuima/jawawiki/resp/CommonResp.java
@@ -1,5 +1,8 @@
 package com.zhuima.jawawiki.resp;
 
+import lombok.Data;
+
+@Data
 public class CommonResp<T> {
 
     /**
@@ -17,37 +20,4 @@ public class CommonResp<T> {
      */
     private T content;
 
-    public boolean getSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public T getContent() {
-        return content;
-    }
-
-    public void setContent(T content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuffer sb = new StringBuffer("ResponseDto{");
-        sb.append("success=").append(success);
-        sb.append(", message='").append(message).append('\'');
-        sb.append(", content=").append(content);
-        sb.append('}');
-        return sb.toString();
-    }
 }
