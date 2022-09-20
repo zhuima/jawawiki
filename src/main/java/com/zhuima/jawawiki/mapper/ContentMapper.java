bcc8c2da39eef3a0c13dfab9c1ead13b41d0cf5a
zhuima <zhuima314@gmail.com>
2022-09-20 13:51:11 +0800

    What: Content代码生成
    Why:
    
    * Content代码生成
    
    How:
    
    * Content代码生成

---
+++ src/main/java/com/zhuima/jawawiki/mapper/ContentMapper.java
@@ -0,0 +1,36 @@
+package com.zhuima.jawawiki.mapper;
+
+import com.zhuima.jawawiki.domain.Content;
+import com.zhuima.jawawiki.domain.ContentExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ContentMapper {
+    long countByExample(ContentExample example);
+
+    int deleteByExample(ContentExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Content record);
+
+    int insertSelective(Content record);
+
+    List<Content> selectByExampleWithBLOBs(ContentExample example);
+
+    List<Content> selectByExample(ContentExample example);
+
+    Content selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByExample(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByPrimaryKeySelective(Content record);
+
+    int updateByPrimaryKeyWithBLOBs(Content record);
+
+}
